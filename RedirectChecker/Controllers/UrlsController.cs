7132bf3d01fde3606d1949639fb7b0548db79194
guavajellyaaron <aaron@guavajellyco.com>
2015-05-04 11:39:27 -0700

    Added initial files to project

---
+++ RedirectChecker/Controllers/UrlsController.cs
@@ -0,0 +1,54 @@
+﻿using RedirectChecker.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Formatting;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Http;
+
+namespace RedirectChecker.Controllers
+{
+    public class UrlsController : ApiController
+    {
+        // Returns the entire HTML
+        //[HttpPost]
+        //public async Task<string> Post([FromBody]MyUrl url) 
+        //{
+        //    using (var httpClient = new HttpClient())
+        //        return await httpClient.GetStringAsync(url.Url);
+        //}
+
+        public string Post([FromBody]MyUrl url)
+        {
+            //return "Returned URL: " + url.Url;
+            if (url.Url != null)
+            {
+                try
+                {
+                    HttpWebRequest webRequest = (HttpWebRequest)WebRequest.Create(url.Url);
+                    webRequest.AllowAutoRedirect = false;
+                    webRequest.Method = "HEAD";
+                    HttpWebResponse response = (HttpWebResponse)webRequest.GetResponse();
+                    return "<tr><td>" + url.Url + "</td><td>" + (int)response.StatusCode + "</td></tr>";
+                }
+                catch (WebException ex)
+                {
+                    string s = "";
+                    HttpWebResponse webResponse = (HttpWebResponse)ex.Response;
+                    if (webResponse.StatusCode == HttpStatusCode.NotFound)
+                    {
+                        s = "<tr><td>" + url.Url + "</td><td>404</td></tr>";
+                    }
+                    return s;
+                }
+            }
+            else
+            {
+                return "";
+            }
+        }
+    }
+}
---
+++ RedirectChecker/Models/MyUrl.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Web;
+
+namespace RedirectChecker.Models
+{
+    public class MyUrl
+    {
+        public string Url { get; set; }
+    }
+}
